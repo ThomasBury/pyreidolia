7b93232c7ebf97f2642e9e68171fc11d61131f9d
Thomas Bury <thomas.bury@allianz.be>
2022-04-27 19:26:47 +0200

    use torch sum

--- pyreidolia/torchloss.py
+++ pyreidolia/torchloss.py
@@ -9,7 +9,7 @@ class DiceLoss(nn.Module):
     def forward(self, inputs, targets, smooth=1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
+        inputs = torch.sigmoid(inputs)       
         
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
@@ -27,14 +27,14 @@ class DiceBCELoss(nn.Module):
     def forward(self, inputs, targets, smooth=1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
+        inputs = torch.sigmoid(inputs)       
         
         #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
+        # inputs = inputs.view(-1)
+        # targets = targets.view(-1)
         
-        intersection = (inputs * targets).sum()                            
-        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        intersection = torch.sum(inputs * targets)                          
+        dice_loss = 1 - (2.*intersection + smooth)/(torch.sum(inputs) + torch.sum(targets) + smooth)  
         # BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
         # unsafe to cast
         BCE = nn.BCEWithLogitsLoss(inputs, targets, reduction='mean')
